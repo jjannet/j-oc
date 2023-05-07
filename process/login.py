698bdc6775974d3653df18f6b959399f03ce0be7
jjannet <jirawat.jannet@gmail.com>
2023-05-07 22:40:54 +0700

    fix tls when login fix check not config user

--- process/login.py
+++ process/login.py
@@ -2,19 +2,21 @@ import subprocess
 import json
 import os
 from process.configUser import readUserConfig
+from enums.colors import bcolors
 
 
 def login():
-    print('startLogin')
     user = readUserConfig()
 
-    if user != None:
+    if user.username and user.password and user.url:
 
         command = "oc login " + user.url + " -u " + \
-            user.username + " -p " + user.password
+            user.username + " -p " + user.password + " --insecure-skip-tls-verify=true"
 
         result = os.system(command)
         print(result)
+    else:
+        print(bcolors.FAIL + "No config user." + bcolors.ENDC)
 
 
 def loginApp():
